154dd52a64c08985f32fcf886a1dda196d564e8a
KarolinaLazakovich <caroline.lazakovich@gmail.com>
2023-11-09 17:25:02 +0300

    homework
    homework_1.8, GIT

--- lesson_1.py
+++ lesson_1.py
@@ -4,7 +4,7 @@
 # целые числа integer 5,6, -521
 #вещественные числа 4.21, 5.61, -9.21
 # boolean True\False 1 0 1 0
-
+"""
 # 1 задание
 print('Задание 1')
 print(17/2*3+2)
@@ -23,18 +23,18 @@ print(17/2%(2*(3**3)))
 
 # 3 задание
 print('Задание 3')
-print((11-(1.5*3)),"рубля")
+print((11-(1.5*3)), "рубля")
 
 # 4 задание
 print('Задание 4')
-Ann_apples=2
-Poll_apples=5
-print(Ann_apples, "яблока","и", Poll_apples, "яблок")
+Ann_apples = 2
+Poll_apples = 5
+print(Ann_apples, "яблока", "и", Poll_apples, "яблок")
 
 # 5 задание
 print('Задание 5')
-days=5
-print("days:",days,"hours:",days*24,"minutes:",days*1440,"sec:",days*86400)
+days = 5
+print("days:", days, "hours:", days*24, "minutes:", days*1440, "sec:", days*86400)
 
 # 6 задание
 print('Задание 6')
@@ -43,13 +43,15 @@ print(182//7)
 # 7 задание
 print('Задание 7')
 print(150//30*65//30, 'квадратов')
+"""
 
 # 8 задание
 print('Задание 8')
-seconds=4000
-print(seconds//3600, 'hour')
-print(seconds//60//10, 'minutes')
-print(seconds//100, 'sec')
+seconds = int(input("Введите количество секунд: "))
+hours = seconds // 3600
+seconds %= 3600
+minutes = seconds // 60
+seconds %= 60
+print("Total:", hours, "hours",",", minutes, "minutes", ",", seconds, "sec")
 
-# 9 задание
-print('Задание 9')
+
